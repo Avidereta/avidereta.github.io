20fdc02641b250e561be51aa231285c6b7ce5aff
anastasia <anastasia@anastasias-MacBook-Pro.local>
2021-10-07 08:23:35 +0100

    fixed some issues with develop and main

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/var/folders/2w/lz5b4f_n3mxgz3pzzwk51d400000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210606130713-db4dcf6541e5/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
